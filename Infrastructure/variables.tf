2b7f5aab3a0718eec4c5df937fe9ff93da74fbe3
Snehasis Sahoo <ssahoo@ajactech.com.au>
2025-04-12 02:08:51 +1000

    variable declaration

--- Infrastructure/variables.tf
+++ Infrastructure/variables.tf
@@ -3,3 +3,18 @@ variable "location" {
   type        = string
   default     = "AustraliaEast"
 }
+
+variable "client_id" {
+  description = "The Client ID of the Azure Service Principal"
+  type        = string
+}
+
+variable "tenant_id" {
+  description = "The Tenant ID of the Azure Active Directory"
+  type        = string
+}
+
+variable "subscription_id" {
+  description = "The Subscription ID for Azure"
+  type        = string
+}
