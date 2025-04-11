35a6ea5913db577080acc5eabe6cfdcd108ee8b2
Snehasis Sahoo <ssahoo@ajactech.com.au>
2025-04-12 01:42:14 +1000

    update

--- Infrastructure/backend.tf
+++ Infrastructure/backend.tf
@@ -4,10 +4,5 @@ terraform {
     storage_account_name = "terraformstatebpb"
     container_name       = "terraform-state"
     key                   = "terraform.tfstate"
-
-    # Use Service Principal authentication here
-    client_id            = var.ARM_CLIENT_ID
-    client_secret        = var.ARM_CLIENT_SECRET
-    tenant_id            = var.ARM_TENANT_ID
   }
 }
--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -12,6 +12,9 @@ terraform {
 provider "azurerm" {
   features {}
   use_oidc        = true
+  client_id       = var.client_id
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
 }
 
 data "azurerm_client_config" "current" {}
